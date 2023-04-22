53ac082ade3a6f6b8b99e2490c6c1b653eb7329f
HYH! <64564727+huangyinhaow@users.noreply.github.com>
2023-04-22 10:06:08 +0800

    fix

--- scripts/download-exe.sh
+++ scripts/download-exe.sh
@@ -10,11 +10,6 @@ check-env NOTION_VERSION NOTION_DOWNLOAD_HASH
 export NOTION_DOWNLOAD_URL="https://desktop-release.notion-static.com/Notion%20Setup%20${NOTION_VERSION}.exe"
 export NOTION_DOWNLOADED_NAME="Notion-${NOTION_VERSION}.exe"
 
-if [ -f "${NOTION_DOWNLOADED_NAME}" ]; then
-  log "Removing already downloaded file..."
-  rm "${NOTION_DOWNLOADED_NAME}"
-fi
-
 log "Downloading Notion Windows package..."
 curl "${NOTION_DOWNLOAD_URL}" --output "${NOTION_DOWNLOADED_NAME}
 
