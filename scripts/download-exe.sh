f70ff1e023abb07ff51eda2772a14d14e5ea2b9f
HYH! <64564727+huangyinhaow@users.noreply.github.com>
2023-04-22 10:20:21 +0800

    fix

--- scripts/download-exe.sh
+++ scripts/download-exe.sh
@@ -4,9 +4,6 @@ set -e
 source `dirname $0`/_utils.sh
 workdir ${WORKSPACE_BUILD_DIR}
 
-check-cmd curl sha512sum
-check-env NOTION_VERSION NOTION_DOWNLOAD_CHECKSUM
-
 export NOTION_DOWNLOAD_URL="https://desktop-release.notion-static.com/Notion%20Setup%20${NOTION_VERSION}.exe"
 export NOTION_DOWNLOADED_NAME="Notion-${NOTION_VERSION}.exe"
 
